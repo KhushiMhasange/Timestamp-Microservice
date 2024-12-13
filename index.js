4dbba0b939c0d45852a17d653fb9cef84d4fef53
Khushi Mhasange <khushimhasange210466@acropolis.in>
2024-12-13 11:38:13 +0000

    api

--- index.js
+++ index.js
@@ -18,10 +18,21 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const ipdate = req.params.date;
+  const date = new Date(ipdate);
+  if(!date) date = Date.now();
+  console.log(date);
+  if(isNaN(date.getTime()) || isNaN(date)){
+    return res.json({error:"Invalid Date"});
+  }
+  const unix = date.getTime();
+  const utc = new Date(date).toUTCString();
+  return res.json({
+    "unix" : unix,
+    "utc" :utc
+  })
 });
 
 
